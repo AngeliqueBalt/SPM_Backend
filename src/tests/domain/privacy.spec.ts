efefe13fe1445a40040499ab8495a68dc73b69a2
Angelique B <angelique@angelbal.com>
2024-04-09 16:25:57 +0100

    added tests for privacy enhancement

--- src/tests/domain/privacy.spec.ts
+++ src/tests/domain/privacy.spec.ts
@@ -1,10 +1,21 @@
 import assert from 'node:assert';
-import { normalise } from '../../domain/privacy';
+import { anonymise, bucketise, normalise } from '../../domain/privacy';
 
 describe('Privacy Enhancing Technologies Tests', () => {
 
     describe('bucketise', () => {
-        // TODO
+
+        it('should return undefined if the number of scores is less than the bucket size', () => {
+            assert.deepStrictEqual(bucketise([1, 2]), undefined);
+        });
+
+        it('should return a single bucket if the number of scores is equal to the bucket size', () => {
+            assert.deepStrictEqual(bucketise([1, 2, 3]), [[1, 2, 3]]);
+        });
+
+        it('should return buckets of the size k, adding the remainder to the last bucket', () => {
+            assert.deepStrictEqual(bucketise([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [[1, 2, 3], [4, 5, 6], [7, 8, 9, 10]]);
+        });
     });
 
     describe('normalise', () => {
@@ -35,7 +46,27 @@ describe('Privacy Enhancing Technologies Tests', () => {
     });
 
     describe('anonymise', () => {
-        // TODO
+
+        it('should return undefined if no buckets are undefined', () => {
+            assert.deepStrictEqual(anonymise([]), undefined);
+        });
+
+        it('should anonymise buckets correctly', () => {
+            assert.deepStrictEqual(anonymise([[1, 2, 3], [4, 5, 6]]), [
+                { minScore: 1, maxScore: 3, proportion: 0.5 },
+                { minScore: 4, maxScore: 6, proportion: 0.5 }
+            ]);
+            assert.deepStrictEqual(anonymise([[1, 2, 3], [4, 5, 6], [7, 8, 9, 10]]), [
+                { minScore: 1, maxScore: 3, proportion: 0.3 },
+                { minScore: 4, maxScore: 6, proportion: 0.3 },
+                { minScore: 7, maxScore: 10, proportion: 0.4 }
+            ]);
+        });
+
+        it("should return undefined if the anonymised bucket's length is less than or equal to 0", () => {
+            assert.deepStrictEqual(anonymise([[]]), undefined);
+        });
+
     });
 
 });
